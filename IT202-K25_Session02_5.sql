11a546af4c63cfd0c5202e53fec5226e515047e5
Nguyễn Hoàng Anh <143367993+dfrgtyhujio@users.noreply.github.com>
2026-04-14 23:14:09 +0700

    Add files via upload

--- IT202-K25_Session02_5.sql
+++ IT202-K25_Session02_5.sql
@@ -1,4 +1,12 @@
--- Giải pháp: Sửa trực tiếp cột
+-- Cách 1: Sửa trực tiếp cột
+-- Cách 2: Thêm cột mới
+
+--             Ưu điểm                  Nhược điểm
+-- C1      Nhanh                     Rủi ro với bảng lớn
+--         Câu lệnh đơn giản
+--
+-- C2      An toàn                   Làm nhiều bước hơn
+
 
 create table users (
 	user_id int primary key,
